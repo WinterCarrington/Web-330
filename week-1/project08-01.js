62d2f27619d5fa6f344de05645c9a641d7092b26
Marguerita <winter.madison82@gmail.com>
2025-10-21 18:21:14 -0400

    renamed file and saved project

---
+++ week-1/project08-01.js
@@ -0,0 +1,15 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 8
+            Project 08-01
+
+                  Project to create a timer object
+                        Author: Lantha Wade
+                              Date:  10/27/25 
+
+                                    Filename: project08-01.js
+                                    */
+
+                                    /*--------------- Object Code --------------------*/
+                    
+
