183e15665ae7410063f3b459fed4a7e431cb6ee1
Marguerita <winter.madison82@gmail.com>
2025-10-21 19:21:16 -0400

    completed step 4, chapter 8

--- week-1/project08-01.js
+++ week-1/project08-01.js
@@ -17,4 +17,32 @@ function timer(min, sec) {
           this.seconds = sec;
              this.timeID = null;
              }
+             // Add the runPause method to the timer prototype //
+             timer.prototype.runPause = function(timer, minBox, secBox) {
+                // Step 5: If timer.timeID is null, start the timer
+                   if (timer.timeID === null) {
+                         timer.minutes = parseInt(minBox.value);
+                               timer.seconds = parseInt(secBox.value);
+
+                                     timer.timeID = setInterval(function() {
+                                              if (timer.seconds > 0) {
+                                                          timer.seconds--;
+                                                                   } else if (timer.minutes > 0) {
+                                                                               timer.minutes--;
+                                                                                           timer.seconds = 59;
+                                                                                                    } else {
+                                                                                                                clearInterval(timer.timeID);
+                                                                                                                            timer.timeID = null;
+                                                                                                                                     }
+
+                                                                                                                                              minBox.value = timer.minutes;
+                                                                                                                                                       secBox.value = timer.seconds;
+                                                                                                                                                             }, 1000);
+                                                                                                                                                                } else {
+                                                                                                                                                                      // Step 6: If timer is running, pause it
+                                                                                                                                                                            clearInterval(timer.timeID);
+                                                                                                                                                                                  timer.timeID = null;
+                                                                                                                                                                                     }
+                                                                                                                                                                                     };
+                                                                                                                                                                                     `7
 
