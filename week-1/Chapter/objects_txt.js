e8992395eec52672570845517e92cf470b66d30b
Marguerita <winter.madison82@gmail.com>
2025-10-21 22:06:01 -0400

    added chapter exercises to folder

---
+++ week-1/Chapter/objects_txt.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*    JavaScript 7th Edition
+      Chapter 8
+      Chapter case   
+
+      Custom Objects Used in Poker Games
+      Author: Lantha Wade 
+      Date:      10/27/25 
+
+      Filename:       objects.js
+ */
