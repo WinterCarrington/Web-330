42ec238cdccf427d448d41272de492238132069c
Marguerita <winter.madison82@gmail.com>
2025-11-04 13:22:19 -0500

    created chapter subfolder exercises

---
+++ week-3/js09b.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+      
+      Eating Well in Season 
+      Author: Lantha Wade 
+      Date:   11/4/25
+      
+      Filename: js09b.js
+ */
