2530ca30226c2fe934621699e909c0d27da9ef27
Marguerita <winter.madison82@gmail.com>
2025-11-17 13:28:16 -0500

    Update script.js

--- week 5/restaurant-reservation/script.js
+++ week 5/restaurant-reservation/script.js
@@ -1,1 +1,26 @@
+/*
+  Pragmatic JavaScript
+  Chapter 2
+  Programming Assignment
 
+  Author: Lantha Wade 
+  Date: 11/24/25
+  Filename: script.js
+*/
+
+// Create an in-memory object array for each table in the restaurant
+let tables = [
+  // Add your table objects here
+];
+
+// Create a function reserveTable
+function reserveTable(tableNumber, callback, time) {
+  // Add your code here
+}
+
+// When the form is submitted, call the reserveTable function
+document
+  .getElementById("reservationForm")
+  .addEventListener("submit", function (e) {
+    // Add your code here
+  });
