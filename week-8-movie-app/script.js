47437f286b39643e7eac9a74df0a74af3733c855
Marguerita <winter.madison82@gmail.com>
2025-12-11 12:29:42 -0500

    Update script.js

--- week-8-movie-app/script.js
+++ week-8-movie-app/script.js
@@ -1,1 +1,23 @@
+/*
+  Pragmatic JavaScript
+  Chapter 4
+  Programming Assignment
 
+  Author: Lantha Wade
+  Date: 12/15/25
+  Filename: script.js
+*/
+
+"use strict";
+
+const movies = [
+  // Add your movie objects here
+];
+
+function fetchMovie(title) {
+  // Implement this function
+}
+
+document.getElementById("movie-form").addEventListener("submit", async (event) => {
+  // Implement this function
+});
