5c736d433af69289121706ac91f316e856ff2b23
Marguerita <winter.madison82@gmail.com>
2025-12-16 16:58:14 -0500

    updated js file

--- week-9/project12-03.js
+++ week-9/project12-03.js
@@ -1,4 +1,4 @@
-"use strict";
+
 /*    JavaScript 7th Edition
       Chapter 12
       Project 12-03
@@ -9,5 +9,28 @@
 
       Filename: project12-03.js
 */
+"use strict";
+
+$("article > h2").click(function () {
+
+   // a. Reference the clicked heading
+   let heading = $(this);
+
+   // b. Reference the next sibling list
+   let list = heading.next();
+
+   // c. Reference the image inside the heading
+   let headingImage = heading.children("img");
+
+   // Toggle list visibility
+   list.slideToggle(500);
+
+   // Switch plus/minus image
+   if (headingImage.attr("src") === "plus.png") {
+      headingImage.attr("src", "minus.png");
+   } else {
+      headingImage.attr("src", "plus.png");
+   }
+});
 
 
